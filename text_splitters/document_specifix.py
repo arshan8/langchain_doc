ef141676c9a9a5d4a0f90c8ff91d32f1834d08ab
Arshan A Shaik <arshanshaik1199@gmail.com>
2025-07-07 23:24:03 +0530

    added text splitters

---
+++ text_splitters/document_specifix.py
@@ -0,0 +1,4 @@
+# #extension of recursive structre splitter,
+
+# but here we can have out spceial type of seprtaors not only \n\n, \n, " ", etc.,
+# we can have class, def, as pythonic sepratos, or for markdown **
---
+++ text_splitters/semantic.py
@@ -0,0 +1,3 @@
+#do not split on the basis of lenght or structure, instead rely on semantic meaning,
+# scentence by scentance, calculuate standard deviation.
+
